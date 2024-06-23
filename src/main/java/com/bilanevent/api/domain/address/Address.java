19ba2ce3a25f3c2f82f397bac348cb2a0d1db062
Thiago-Bilangieri <tbilangieri@gmail.com>
2024-06-23 14:53:50 +0100

    feat: creating entities

---
+++ src/main/java/com/bilanevent/api/domain/address/Address.java
@@ -0,0 +1,19 @@
+package com.bilanevent.api.domain.address;
+
+import com.bilanevent.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String city:
+    private String uf;
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/bilanevent/api/domain/coupon/Coupon.java
@@ -0,0 +1,30 @@
+package com.bilanevent.api.domain.coupon;
+
+
+import com.bilanevent.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
